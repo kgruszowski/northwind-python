b8f8f5eff121358a7143aa5d7c8d1e8cc53c1562
maciej ksiazek <zrzut01@poczta.fm>
2017-12-14 01:48:58 +0100

    Order detailed view particular products and total order price computation added

--- store/orders/views.py
+++ store/orders/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.db.models import Sum
 
 from customers.models import Orders, Shippers, Suppliers 
 
@@ -18,3 +18,8 @@ class DetailView(generic.DetailView):
         model = Orders
         template_name = 'orders/detail.html'
 
+        def totalPrice(self):
+            total = 0
+            for i in self.get_object().orderdetails_set.values():
+                total += i.get('quantity') * i.get('unitprice')
+            return total
